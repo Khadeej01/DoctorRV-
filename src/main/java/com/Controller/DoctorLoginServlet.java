b85494c0d6883050724dd16ebe0187f68f3423ec
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-21 11:44:41 +0100

    Doctor servlet

---
+++ src/main/java/com/Controller/DoctorLoginServlet.java
@@ -0,0 +1,63 @@
+package com.Controller;
+
+import com.DAO.DoctorDAO;
+import com.Model.Doctor;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/")
+public class DoctorLoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private DoctorDAO doctorDAO;
+
+    public void init() {
+        doctorDAO = new DoctorDAO();
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+
+        try {
+            switch (action) {
+
+                case "/test":
+
+                    break;
+                default:
+                    listDoctor(request, response);
+
+                    break;
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    private void listDoctor(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List< Doctor > listDoctor = doctorDAO.selectAllDocters();
+        request.setAttribute("listDoctor", listDoctor);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("./");
+        dispatcher.forward(request, response);
+    }
+
+
+
+}
