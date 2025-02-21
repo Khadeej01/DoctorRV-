25ad5612be12014a07c591421a4cdaa9a2c90cbc
khadijaDev <khadijaoukhlift@gmail.com>
2025-02-21 11:48:07 +0100

    data base connection

--- src/main/java/com/DAO/DBConnection.java
+++ src/main/java/com/DAO/DBConnection.java
@@ -1,43 +1,43 @@
-//package com.DAO;
-//
-//import com.Model.Appointment;
-//import com.Model.Doctor;
-//import com.Model.Patient;
-//
-//import java.sql.*;
-//
-//public class DBConnection {
-//    private static final String URL = "jdbc:mysql://localhost:3306/DoctorRV";
-//    private static final String USER = "root";
-//    private static final String PASSWORD = "";
-//
-//
-//    public static Connection getConnection() {
-//        Connection conn = null;
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return conn;
-//    }
-//    public void addPatient(Patient patient) {
-//        String sql = "INSERT INTO Patient (usename, Email, telephone, motif) VALUES (?, ?, ?, ?)";
-//
-//        try (Connection conn = DBConnection.getConnection();
-//             PreparedStatement stmt = conn.prepareStatement(sql)) {
-//
-//            stmt.setString(1, patient.getUsername());
-//            stmt.setString(2, patient.getEmail());
-//            stmt.setInt(3, patient.getTelephone());
-//            stmt.setString(4, patient.getMotif());
-//
-//            stmt.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+package com.DAO;
+
+import com.Model.Appointment;
+import com.Model.Doctor;
+import com.Model.Patient;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/DoctorRV";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    public void addPatient(Patient patient) {
+        String sql = "INSERT INTO Patient (usename, Email, telephone, motif) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, patient.getUsername());
+            stmt.setString(2, patient.getEmail());
+            stmt.setInt(3, patient.getTelephone());
+            stmt.setString(4, patient.getMotif());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 //    public Patient getPatientById(int id) {
 //        String sql = "SELECT * FROM Patient WHERE id = ?";
 //        Patient patient = null;
@@ -76,7 +76,7 @@
 //            e.printStackTrace();
 //        }
 //    }
-//}
+}
 //
 //
 //
